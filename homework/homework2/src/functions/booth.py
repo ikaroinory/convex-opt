e41f7a10ee4ac06b823317b848bdd3f405184d49
Guanyu Hu <ikaroinory@gmail.com>
2025-04-02 10:26:24 +0800

    feat: 添加了一些测试函数的梯度

--- homework/homework2/src/functions/booth.py
+++ homework/homework2/src/functions/booth.py
@@ -14,3 +14,21 @@ def booth(data: torch.Tensor) -> torch.Tensor:
     y = data[..., 1]
 
     return (x + 2 * y - 7) ** 2 + (2 * x + y - 5) ** 2
+
+
+def booth_grad(data: torch.Tensor) -> torch.Tensor:
+    """
+
+    Gradient of Booth function.
+
+    :param data: The input tensor with shape of (*, 2).
+    :return: The gradient tensor with shape of (*, 2).
+    """
+
+    x = data[..., 0]
+    y = data[..., 1]
+
+    grad_x = 10 * x + 8 * y - 34
+    grad_y = 8 * x + 10 * y - 38
+
+    return torch.stack([grad_x, grad_y], dim=-1)
--- homework/homework2/src/functions/wheeler.py
+++ homework/homework2/src/functions/wheeler.py
@@ -15,3 +15,22 @@ def wheeler(data: torch.Tensor, a=1.5) -> torch.Tensor:
     y = data[..., 1]
 
     return -torch.exp(-(x * y - a) ** 2 - (y - a) ** 2)
+
+
+def wheeler_grad(data: torch.Tensor, a=1.5) -> torch.Tensor:
+    """
+
+    Wheeler function gradient.
+
+    :param data: The input tensor with shape of (*, 2).
+    :param a: Parameter.
+    :return: Wheeler function gradient with shape of (*, 2).
+    """
+
+    x = data[..., 0]
+    y = data[..., 1]
+
+    grad_x = 2 * (x * y - a) * y * torch.exp(-(x * y - a) ** 2 - (y - a) ** 2)
+    grad_y = torch.exp(-(x * y - a) ** 2 - (y - a) ** 2) * (-2 * a * (1 + x) + 2 * y * (1 + x ** 2))
+
+    return torch.stack([grad_x, grad_y], dim=-1)
