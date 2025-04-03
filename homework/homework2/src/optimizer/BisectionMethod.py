711af5882c887c49ec059b6c5d620536640eb9ec
Guanyu Hu <ikaroinory@gmail.com>
2025-04-03 14:36:25 +0800

    feat: 添加二分优化器

---
+++ homework/homework2/src/optimizer/BisectionMethod.py
@@ -0,0 +1,25 @@
+import torch
+
+from optimizer.Optimizer import Optimizer
+
+
+class BisectionMethod(Optimizer):
+    def __init__(self, f, f_grad=None, epsilon=1e-6):
+        super(BisectionMethod, self).__init__(f, f_grad, epsilon=epsilon)
+
+    def optimize(self, alpha: torch.Tensor, beta: torch.Tensor) -> torch.Tensor:
+        while beta - alpha >= self.epsilon:
+            x = (alpha + beta) / 2
+            f_grad_x = self.grad_f(x) if self.grad_f else self.f(x) - self.f(x + self.epsilon ** 2)
+
+            if f_grad_x == 0:
+                break
+
+            if f_grad_x > 0:
+                beta = x
+            else:
+                alpha = x
+
+            self.iterator_count += 1
+
+        return (alpha + beta) / 2
--- homework/homework2/src/optimizer/Optimizer.py
+++ homework/homework2/src/optimizer/Optimizer.py
@@ -4,9 +4,10 @@ import torch
 
 
 class Optimizer:
-    def __init__(self, f: Callable[[torch.Tensor], torch.Tensor], grad_f: Callable[[torch.Tensor], torch.Tensor] = None):
+    def __init__(self, f: Callable[[torch.Tensor], torch.Tensor], grad_f: Callable[[torch.Tensor], torch.Tensor] = None, epsilon=1e-6):
         self.f = f
         self.grad_f = grad_f
+        self.epsilon = epsilon
 
         self.iterator_count = 0
 
