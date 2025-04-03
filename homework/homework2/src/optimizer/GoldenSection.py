f92c66aa683b3a253ce3dc150b61b1692cd255a6
Guanyu Hu <ikaroinory@gmail.com>
2025-04-03 14:25:07 +0800

    opt: 优化了黄金分割优化器

--- homework/homework2/src/optimizer/GoldenSection.py
+++ homework/homework2/src/optimizer/GoldenSection.py
@@ -4,28 +4,21 @@ from optimizer.Optimizer import Optimizer
 
 
 class GoldenSection(Optimizer):
-    def __init__(self, f, max_iter=100):
+    def __init__(self, f):
         super(GoldenSection, self).__init__(f)
-
-        self.max_iter = max_iter
 
     def optimize(self, alpha: torch.Tensor, beta: torch.Tensor, epsilon=1e-6) -> torch.Tensor:
         t = (torch.sqrt(torch.tensor(5)) - 1) / 2
 
-        left = alpha + (1 - t) * (beta - alpha)
-        right = alpha + t * (beta - alpha)
-
-        for self.iterator_count in range(self.max_iter):
-            if beta - alpha < epsilon:
-                break
+        while beta - alpha >= epsilon:
+            left = alpha + (1 - t) * (beta - alpha)
+            right = alpha + t * (beta - alpha)
 
             if self.f(left) - self.f(right) > 0:
                 alpha = left
-                left = right
-                right = alpha + t * (beta - alpha)
             else:
                 beta = right
-                right = left
-                left = alpha + (1 - t) * (beta - alpha)
+
+            self.iterator_count += 1
 
         return (alpha + beta) / 2
